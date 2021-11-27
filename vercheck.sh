18403a7c5e9a7dd5c016839d605e1b92ca3c7f78
Nate Sales <nate@natesales.net>
2021-11-26 21:25:20 -0800

    refactor: cleanup vercheck

--- vercheck.sh
+++ vercheck.sh
@@ -4,7 +4,7 @@
 # TODO: Add docker tag checker
 
 symverlt() {
-    [ "$1" = "$2" ] && return 1 || [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+    [ "$1" = "$2" ] && return 1 || [  "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
 }
 
 API_VERSION=$(yq -r .all.vars.config.controlplane.versions.docker.api hosts.yml)
@@ -13,13 +13,24 @@ WEB_VERSION=$(yq -r .all.vars.config.controlplane.versions.archives.web hosts.ym
 LATEST_API_VERSION=$(curl https://api.github.com/repos/packetframe/api/releases/latest -s | jq .name -r | sed 's/v//')
 LATEST_WEB_VERSION=$(curl https://api.github.com/repos/packetframe/web/releases/latest -s | jq .name -r | sed 's/v//')
 
-symverlt $API_VERSION $LATEST_API_VERSION && echo "API out of date ($API_VERSION < $LATEST_API_VERSION)" || echo "API version up to date ($API_VERSION)"
-symverlt $WEB_VERSION $LATEST_WEB_VERSION && echo "Web out of date ($WEB_VERSION < $LATEST_WEB_VERSION)" || echo "Web version up to date ($WEB_VERSION)"
+if symverlt $API_VERSION $LATEST_API_VERSION; then
+  echo "API out of date ($API_VERSION < $LATEST_API_VERSION)"
 
-if [[ "$1" == "fix" ]]; then
-  echo "Fixing..."
-  sed -i "s/api: $API_VERSION/api: $LATEST_API_VERSION/" hosts.yml
-  sed -i "s/web: $WEB_VERSION/web: $LATEST_WEB_VERSION/" hosts.yml
-  symverlt $API_VERSION $LATEST_API_VERSION && echo "API out of date ($API_VERSION < $LATEST_API_VERSION)" || echo "API version up to date ($API_VERSION)"
-  symverlt $WEB_VERSION $LATEST_WEB_VERSION && echo "Web out of date ($WEB_VERSION < $LATEST_WEB_VERSION)" || echo "Web version up to date ($WEB_VERSION)"
+  if [[ "$1" == "fix" ]]; then
+    sed -i "s/api: $API_VERSION/api: $LATEST_API_VERSION/" hosts.yml
+    echo "Updated API from $API_VERSION to $LATEST_API_VERSION"
+  fi
+else
+  echo "API version up to date ($API_VERSION)"
+fi
+
+if symverlt $WEB_VERSION $LATEST_WEB_VERSION; then
+  echo "Web out of date ($WEB_VERSION < $LATEST_WEB_VERSION)"
+
+  if [[ "$1" == "fix" ]]; then
+    sed -i "s/web: $WEB_VERSION/web: $LATEST_WEB_VERSION/" hosts.yml
+    echo "Updated web from $WEB_VERSION to $LATEST_WEB_VERSION"
+  fi
+else
+  echo "Web version up to date ($WEB_VERSION)"
 fi
