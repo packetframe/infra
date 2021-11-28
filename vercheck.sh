d5c0732f3c1cd16958978c8e9becceb9b1cf980f
Nate Sales <nate@natesales.net>
2021-11-28 00:35:20 -0800

    feat: extract public configs to files

--- vercheck.sh
+++ vercheck.sh
@@ -3,12 +3,14 @@
 
 # TODO: Add docker tag checker
 
+CONFIG_FILE=config/controlplane-versions.yml
+
 symverlt() {
     [ "$1" = "$2" ] && return 1 || [  "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
 }
 
-API_VERSION=$(yq -r .all.vars.config.controlplane.versions.docker.api hosts.yml)
-WEB_VERSION=$(yq -r .all.vars.config.controlplane.versions.archives.web hosts.yml)
+API_VERSION=$(yq -r .controlplane_versions.docker.api $CONFIG_FILE)
+WEB_VERSION=$(yq -r .controlplane_versions.archives.web $CONFIG_FILE)
 
 LATEST_API_VERSION=$(curl https://api.github.com/repos/packetframe/api/releases/latest -s | jq .name -r | sed 's/v//')
 LATEST_WEB_VERSION=$(curl https://api.github.com/repos/packetframe/web/releases/latest -s | jq .name -r | sed 's/v//')
@@ -17,7 +19,7 @@ if symverlt $API_VERSION $LATEST_API_VERSION; then
   echo "API out of date ($API_VERSION < $LATEST_API_VERSION)"
 
   if [[ "$1" == "fix" ]]; then
-    sed -i "s/api: $API_VERSION/api: $LATEST_API_VERSION/" hosts.yml
+    sed -i "s/api: $API_VERSION/api: $LATEST_API_VERSION/" $CONFIG_FILE
     echo "Updated API from $API_VERSION to $LATEST_API_VERSION"
   fi
 else
@@ -28,7 +30,7 @@ if symverlt $WEB_VERSION $LATEST_WEB_VERSION; then
   echo "Web out of date ($WEB_VERSION < $LATEST_WEB_VERSION)"
 
   if [[ "$1" == "fix" ]]; then
-    sed -i "s/web: $WEB_VERSION/web: $LATEST_WEB_VERSION/" hosts.yml
+    sed -i "s/web: $WEB_VERSION/web: $LATEST_WEB_VERSION/" $CONFIG_FILE
     echo "Updated web from $WEB_VERSION to $LATEST_WEB_VERSION"
   fi
 else
