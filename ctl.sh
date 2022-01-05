e6ddbbb148b9884491f3cbb4261c1abf54f74fb9
Nate Sales <nate@natesales.net>
2022-01-04 18:15:43 -0800

    chore: combine scripts

--- ctl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Packetframe infra ctl.sh
-
-ROLE_NAMESPACE=controlplane
-ROLE_NAME=firewall
-HOST=controlplane
-ansible -m import_role -a name=./roles/$ROLE_NAMESPACE/$ROLE_NAME $HOST -i hosts.yml
