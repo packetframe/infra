2ae18c3450bfd1fa67d25ae120d13b6e2b5a15f0
Nate Sales <nate@natesales.net>
2022-01-06 23:17:37 -0800

    feat: add as112

--- roles/edge/system/templates/net.sh
+++ roles/edge/system/templates/net.sh
@@ -7,4 +7,15 @@ ip addr add dev local 66.248.234.3/24 # ns2
 ip addr add dev local 2602:809:3004::3/48 # ns2
 ip addr add dev local 66.248.234.5/24 # Verfploeter
 ip addr add dev local 2602:809:3004::5/48 # Verfploeter
+
+# AS112
+ip addr add dev local 192.175.48.1/32       #  prisoner.iana.org
+ip addr add dev local 2620:4f:8000::1/128   #  prisoner.iana.org
+ip addr add dev local 192.175.48.6/32       #  blackhole-1.iana.org
+ip addr add dev local 2620:4f:8000::6/128   #  blackhole-1.iana.org
+ip addr add dev local 192.175.48.42/32      #  blackhole-2.iana.org
+ip addr add dev local 2620:4f:8000::42/128  #  blackhole-2.iana.org
+ip addr add dev local 192.31.196.1/32       #  blackhole.as112.arpa
+ip addr add dev local 2001:4:112::1/128     #  blackhole.as112.arpa
+
 ip link set dev local up
