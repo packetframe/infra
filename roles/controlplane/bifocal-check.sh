4f508e002cdb36ee94ff6e70598e6ad45ed5eb52
Nate Sales <nate@natesales.net>
2022-04-02 16:25:26 -0700

    fix: disable IPv6 bifocal check

--- roles/controlplane/bifocal-check.sh
+++ roles/controlplane/bifocal-check.sh
@@ -6,7 +6,8 @@ DIG_COMMAND="dig +time=5 +tries=1 +nsid CH id.server TXT"
 MTR_COMMAND="mtr --show-ips --report-wide --aslookup"
 PROMPT="$"
 
-for target in 66.248.234.2 66.248.234.3 2602:809:3004::2 2602:809:3004::3; do
+# for target in 66.248.234.2 66.248.234.3 2602:809:3004::2 2602:809:3004::3; do
+for target in 66.248.234.2 66.248.234.3; do
   dig_result=$($DIG_COMMAND @"$target")
   RESULT=$?
   mtr_result=$($MTR_COMMAND $target 2>&1)
