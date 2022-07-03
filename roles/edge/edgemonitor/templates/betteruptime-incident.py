97d9ba03dd973866ca67a7d57280e72366ab87ee
Nate Sales <nate@natesales.net>
2022-07-02 18:26:24 -0700

    feat: send email on edgemonitor

--- roles/edge/edgemonitor/templates/betteruptime-incident.py
+++ roles/edge/edgemonitor/templates/betteruptime-incident.py
@@ -7,6 +7,6 @@ BETTERUPTIME_APIKEY = "{{ config.betteruptime_apikey }}"
 
 r = requests.post(
     "https://betteruptime.com/api/v2/incidents",
-    json={"summary": sys.argv[1], "requester_email": "noreply@packetframe.com", "description": sys.stdin.read()},
+    json={"summary": sys.argv[1], "requester_email": "noreply@packetframe.com", "description": sys.stdin.read(), "email": True},
     headers={"Authorization": "Bearer " + BETTERUPTIME_APIKEY, "Content-Type": "application/json"}
 )
